dc751890abb334aba7491887a19f2c21e2d23023
periaate <daniel.saury.00@gmail.com>
2023-08-03 21:05:25 +0300

    native thumbnail creation

--- thumb.go
+++ thumb.go
@@ -3,9 +3,13 @@ package media
 import (
 	"bytes"
 	"errors"
+	"image"
+	"image/jpeg"
 	"io"
 	"os"
 	"os/exec"
+
+	"golang.org/x/image/draw"
 )
 
 var ErrFFMPEGNotFound = errors.New("ffmpeg not found in PATH")
@@ -44,6 +48,14 @@ func RunFfmpeg(r io.Reader, w io.Writer, args ...string) error {
 	return cmd.Run()
 }
 
+func RawRunFfmpeg(args ...string) error {
+	if ffmpegNotFound {
+		return ErrFFMPEGNotFound
+	}
+	cmd := exec.Command("ffmpeg", args...)
+	return cmd.Run()
+}
+
 func init() {
 	_, err := exec.LookPath("ffmpeg")
 	if err != nil {
@@ -53,3 +65,31 @@ func init() {
 
 // Todo: Implement a function which uses image.Image instead of ffmpeg.
 // This will require less dependencies, and will likely be faster.
+
+func CreateThumbnailImage(img image.Image, w io.Writer) error {
+	const maxDimension = 1024
+
+	// Calculate new dimensions while maintaining aspect ratio
+	srcWidth := img.Bounds().Dx()
+	srcHeight := img.Bounds().Dy()
+	newWidth, newHeight := srcWidth, srcHeight
+
+	if srcWidth > maxDimension || srcHeight > maxDimension {
+		if srcWidth > srcHeight {
+			newWidth = maxDimension
+			newHeight = (srcHeight * maxDimension) / srcWidth
+		} else {
+			newHeight = maxDimension
+			newWidth = (srcWidth * maxDimension) / srcHeight
+		}
+	}
+
+	// Create a new image with the calculated dimensions
+	dst := image.NewRGBA(image.Rect(0, 0, newWidth, newHeight))
+
+	// Scale the image using the ApproxBiLinear interpolation algorithm
+	draw.ApproxBiLinear.Scale(dst, dst.Bounds(), img, img.Bounds(), draw.Src, nil)
+
+	// Encode the image as JPEG and write to the writer
+	return jpeg.Encode(w, dst, &jpeg.Options{Quality: 50})
+}
